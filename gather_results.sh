22027b8b70a3eec14dde6da976b5f896939f167c
ssari-memory <sercan.sari@std.yeditepe.edu.tr>
2022-08-16 13:59:02 +0300

    formatted results for thread 0 4-thread commit

--- gather_results.sh
+++ gather_results.sh
@@ -4,7 +4,7 @@ i="0"
 len=78
 while [ $i -le $len ]
 do
-	grep "TH0" "$i".txt >> result."$i".txt
+	grep "TH0" "$i".txt | awk '{printf("%s %s %s %s\n", $3, $5, $7, $9)}' >> result_new."$i".txt
 	i=$(expr $i + 1)
 done
 
